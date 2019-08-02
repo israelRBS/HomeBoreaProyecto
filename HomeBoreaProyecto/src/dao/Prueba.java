75a9f41b9ba44acc491bc05c7a94abb00d922e76
israelRBS <armando123batz@gmail.com>
2019-08-02 10:38:46 -0600

    Prueba
    Prueba

--- HomeBoreaProyecto/src/dao/Prueba.java
+++ HomeBoreaProyecto/src/dao/Prueba.java
@@ -16,7 +16,7 @@ public class Prueba {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-    ConexionRandal conexion=new ConexionRandal();
+    ConexionBorea conexion=new ConexionBorea();
     conexion.abrirConexion();
     conexion.cerrarConexion();
         System.out.println("hola a todos");
